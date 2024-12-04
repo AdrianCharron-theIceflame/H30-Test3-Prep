6f1282fdf7ebaf73bc9fed75ef87e02ccceecc65
Adrian Charron - theIceflame <adriancharron@yahoo.ca>
2024-12-04 14:54:03 -0500

    Starting to fetch data from proxy

--- TypescriptApp/my-app/src/Components/App.tsx
+++ TypescriptApp/my-app/src/Components/App.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from 'react';
 import '../Styles/App.css';
 import Store from './Store';
 
+type ProductListType = {
+  ProductId: number,
+  StockNumber: string,
+  Description: string,
+  Inventory: number,
+  Cost: number
+}[]
+
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Super Store</h1>
-      <Store products={productsList} />
-    </div>
-  );
+  const [productsList, setProductsList] = useState<ProductListType | null>(null)
+  useEffect(() => {
+    (async function () => {
+      
+    })()
+}, [])
+return (
+  <div className="App">
+    <h1>Super Store</h1>
+    {productsList != null ? <Store products={productsList as ProductListType} /> : <h2>We're sorry, we could not find the list of products!</h2>}
+  </div>
+);
 }
